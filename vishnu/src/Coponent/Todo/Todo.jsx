92d35cfed0eb94b17cf11e1fc74394a4b966baa3
Vishnu Kumar <vermaji1891@gmail.com>
2023-09-19 18:06:21 +0530

    Todo App Complete

--- vishnu/src/Coponent/Todo/Todo.jsx
+++ vishnu/src/Coponent/Todo/Todo.jsx
@@ -3,54 +3,42 @@ import './Todo.css';
 import TodoList from '../TodoList/TodoList';
 
 function Todo ({todolist}) {
-    const [todoItems, setTodoItems] = useState([{data: "Vishnu", id: 23}, {data: "Neha", id: 45}])
+    const [todoItems, setTodoItems] = useState([{data:"Vishnu", id: 26},]);
     const [inputText, setInputText] = useState("");
-
-
-    function deleteTodo (todoId){
-        const remainingTodos = todoItems.filter((todo) => todo.id != todoId);
-        setTodoItems(remainingTodos);
+    
+    function deleteTodo(id) {
+        const remainingTodo = todoItems.filter((todo) => todo.id != id);
+        setTodoItems(remainingTodo);
     }
-
-    function editTodo(id, newTodo) {
+    function editTodo (id, newTodo) {
         let updatedTodos = todoItems.map((todo) => {
-            if(todo.id == id) {
-                console.log("condition matched")
-                todo.data = newTodo
-            }
-            return todo;
-        })
-
-        setTodoItems(updatedTodos)
+            if(todo.id == id) todo.data = newTodo;
+            return todo; 
+        });
+        setTodoItems(updatedTodos);
     }
-
-
     return (
-        <>
-            <div className='input-wrapper'>
-                    <h1>This is a todo app</h1>
-                    <input className='inp' type="text" placeholder="Your Task Here..."
-                        value={inputText}
-                        onChange={(e) => setInputText(e.target.value)}
-                    />
-                    <button className='btn'
-                        onClick={() => setTodoItems([ ...todoItems, {data: inputText, id: (new Date()).getTime()}])}
-                    >Add Todo</button>
-            </div>
-            {todoItems.map((todo) => <TodoList 
-            
-                                    key={todo.id}
-                                    todoData={todo.data}
-                                    deleteTask={() => deleteTodo(todo.id)}
-                                    edit={(todoData) => editTodo(todo.id, todoData)}
+        <div className="input-wrapper">  
+            <h1>This is Todo App</h1>
+            <input 
+                className="inp"
+                type="text" 
+                placeholder="Your Task Here..."
+                value={inputText}  
+                onChange={(e) => setInputText(e.target.value)}
             />
-
-                
-
-             )}
-             
+            <button className="btn" onClick={() => {
+                setTodoItems([...todoItems, {data:inputText, id: (new Date()).getTime()}])
+                setInputText("")
+            } }>Add ToDo</button>
+            {todoItems.map((todo) => <TodoList 
+            key={todo.id} 
+            todo={todo}
+            delete={() => deleteTodo(todo.id)}
+            edit={(newTodo) => editTodo(todo.id, newTodo)}
+            />)}
             
-        </>
+        </div>
     )
 }
 
--- vishnu/src/Coponent/TodoList/TodoList.jsx
+++ vishnu/src/Coponent/TodoList/TodoList.jsx
@@ -2,33 +2,35 @@ import { useState } from 'react';
 import './TodoList.css'
 
 function TodoList (props) {
-    // const [edit, setEdit] = useState(false);
     const [isEdit, setIsEdit] = useState(false)
-    const [todoDatas, setTodoDatas] = useState(props.todoData)
-    console.log("button state is", isEdit)
+    const [todoData, setTodoData] = useState(props.todo.data);
     return (
-        <div  className="todolist-wrapper">
+        <div className="todolist-wrapper"> 
             <div>
                 {
-                (isEdit) ? <input  type='text' className='inp'
-                value={todoDatas} onChange={(e) => setTodoDatas(e.target.value)}  /> :
-                <h2>{props.todoData}</h2>
+                    (isEdit) ? <input type="text" className="inp"
+                    value={todoData} 
+                    onChange={(e) => setTodoData(e.target.value)}/> 
+                    : <h2 className="task">{props.todo.data}</h2>                        
                 }
             </div>
-
-            <div className='btn-wrapper'>
-                
-                <button className="btn-2" onClick={() => {
-                    setIsEdit(!false)
-                    props.edit(todoDatas);
-                }}>{(isEdit) ? "Save" : "Edit"}</button>
-
-                <button className="btn-2"
-                    onClick={() => {
-                        props.deleteTask()
-                    }}
-                >Delete</button>
-
+            
+            <div className="btn-wrapper">
+                <button 
+                className="btn-2"
+                onClick={() => {
+                    props.delete();
+                }
+                } 
+                > Delete </button>
+                <button
+                className="btn-2"
+                onClick={() => {
+                    setIsEdit(!isEdit) 
+                    props.edit(todoData)
+                }
+                }
+                >{(isEdit) ? "Save" : "Edit"}</button>
             </div>
         </div>
     )
